5bc75be1f295c166be4b3c820f2d95e10843e576
Kamil Hismatullin <kamil.hismatullin@flatstack.com>
2022-01-28 18:02:34 +0100

    setup&run eslint and prettier

--- services/ptos_fetcher/src/app.ts
+++ services/ptos_fetcher/src/app.ts
@@ -1,56 +1,61 @@
+import * as dotenv from 'dotenv';
 import PTOsFetcher from './fetchers/PTOsFetcher';
 import UsersFetcher from './fetchers/UsersFetcher';
 import PTO from './models/PTO';
 import User from './models/User';
-import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 export const TOKEN = process.env.NOTION_TOKEN;
 
-export const lambdaHandler = async () => {
+export function todayPlus(plus: number): string {
+  const today = new Date();
+  return new Date(today.getFullYear(), today.getMonth(), today.getDate() + plus)
+    .toISOString()
+    .slice(0, 10);
+}
 
+export const lambdaHandler = async () => {
   const today = todayPlus(1);
   const todayPlusThreeDays = todayPlus(4);
 
-  const ptosFetcher = new PTOsFetcher(
-    today,
-    todayPlusThreeDays,
-    TOKEN,
-  );
+  const ptosFetcher = new PTOsFetcher(today, todayPlusThreeDays, TOKEN);
   const ptos = await ptosFetcher.fetch();
 
   const usersFetcher = new UsersFetcher(
     ptos.map((pto) => pto.uid),
-    TOKEN,
+    TOKEN
   );
   const users = await usersFetcher.fetch();
 
-  const widgetData:
-    Record<string, Record<string, { full_name: string | undefined, profile_photo: string | undefined }>> = {};
+  const widgetData: Record<
+    string,
+    Record<
+      string,
+      { full_name: string | undefined; profile_photo: string | undefined }
+    >
+  > = {};
+
   [1, 2, 3, 4].forEach((daysFromToday: number) => {
     const date = todayPlus(daysFromToday);
     widgetData[date] = {};
     ptos.forEach((pto: PTO) => {
-      if (pto.startDate == date || pto.endDate == date || (pto.startDate < date && pto.endDate > date)) {
-        const user = users.find((user: User) => { return user.uid == pto.uid })
+      if (
+        pto.startDate === date ||
+        pto.endDate === date ||
+        (pto.startDate < date && pto.endDate > date)
+      ) {
+        const user = users.find((u: User) => u.uid === pto.uid);
         widgetData[date][pto.uid] = {
           full_name: user?.full_name,
-          profile_photo: user?.profile_photo
-        }
+          profile_photo: user?.profile_photo,
+        };
       }
     });
   });
   // clean dynamodb
   // push new data to dynamodb
-  console.log(widgetData)
-}
-
-export function todayPlus(plus: number): string {
-  const today = new Date();
-  return new Date(
-    today.getFullYear(), today.getMonth(), today.getDate() + plus
-  ).toISOString().slice(0, 10);
-}
+  console.log(widgetData);
+};
 
 lambdaHandler();
--- services/ptos_fetcher/src/fetchers/PTOsFetcher.ts
+++ services/ptos_fetcher/src/fetchers/PTOsFetcher.ts
@@ -1,9 +1,11 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from 'axios';
 import PTO from '../models/PTO';
 
 export default class PTOsFetcher {
   fromDate!: string;
+
   toDate!: string;
+
   token!: string | undefined;
 
   constructor(fromDate: string, toDate: string, token: string | undefined) {
@@ -14,20 +16,22 @@ export default class PTOsFetcher {
 
   async fetch(): Promise<Array<PTO>> {
     const ptosContent = await this.queryCollection();
-    const blockIds = ptosContent.data.result.reducerResults.calendar_results.blockIds;
+    const { blockIds } =
+      ptosContent.data.result.reducerResults.calendar_results;
     const ptos: PTO[] = [];
     blockIds.forEach((blockId: string) => {
-      const blockProps = ptosContent.data.recordMap.block[blockId].value.properties;
-      const dates = blockProps["2`kt"][0][1][0][1];
-      blockProps["s.DD"].forEach((blockUser: string) => {
-        if (blockUser.length == 1) {
-          return
+      const blockProps =
+        ptosContent.data.recordMap.block[blockId].value.properties;
+      const dates = blockProps['2`kt'][0][1][0][1];
+      blockProps['s.DD'].forEach((blockUser: string) => {
+        if (blockUser.length === 1) {
+          return;
         }
         ptos.push({
-          startDate: dates["start_date"],
-          endDate: dates["end_date"] || dates["start_date"],
-          uid: blockUser[1][0][1]
-        })
+          startDate: dates.start_date,
+          endDate: dates.end_date || dates.start_date,
+          uid: blockUser[1][0][1],
+        });
       });
     });
     return ptos;
@@ -35,66 +39,66 @@ export default class PTOsFetcher {
 
   private async queryCollection(): Promise<AxiosResponse> {
     const calendarQueryBody = {
-      "collection": {
-        "id": "1a3c17b7-1897-471d-96b0-7ed7c6548d46",
-        "spaceId": "c4882ad3-57cd-492b-a5c0-ca55f47745b5"
+      collection: {
+        id: '1a3c17b7-1897-471d-96b0-7ed7c6548d46',
+        spaceId: 'c4882ad3-57cd-492b-a5c0-ca55f47745b5',
       },
-      "collectionView": {
-        "id": "69b269d1-67a0-4e1e-9a24-e978e93ae065",
-        "spaceId": "c4882ad3-57cd-492b-a5c0-ca55f47745b5"
+      collectionView: {
+        id: '69b269d1-67a0-4e1e-9a24-e978e93ae065',
+        spaceId: 'c4882ad3-57cd-492b-a5c0-ca55f47745b5',
       },
-      "loader": {
-        "type": "reducer",
-        "reducers": {
-          "calendar_results": {
-            "type": "results",
-            "filter": {
-              "operator": "and",
-              "filters": [
+      loader: {
+        type: 'reducer',
+        reducers: {
+          calendar_results: {
+            type: 'results',
+            filter: {
+              operator: 'and',
+              filters: [
                 {
-                  "property": "2`kt",
-                  "filter": {
-                    "operator": "date_is_on_or_after",
-                    "use_end": true,
-                    "value": {
-                      "type": "exact",
-                      "value": {
-                        "type": "date",
-                        "start_date": this.fromDate
-                      }
-                    }
-                  }
+                  property: '2`kt',
+                  filter: {
+                    operator: 'date_is_on_or_after',
+                    use_end: true,
+                    value: {
+                      type: 'exact',
+                      value: {
+                        type: 'date',
+                        start_date: this.fromDate,
+                      },
+                    },
+                  },
                 },
                 {
-                  "property": "2`kt",
-                  "filter": {
-                    "operator": "date_is_on_or_before",
-                    "value": {
-                      "type": "exact",
-                      "value": {
-                        "type": "date",
-                        "start_date": this.toDate
-                      }
-                    }
-                  }
-                }
-              ]
+                  property: '2`kt',
+                  filter: {
+                    operator: 'date_is_on_or_before',
+                    value: {
+                      type: 'exact',
+                      value: {
+                        type: 'date',
+                        start_date: this.toDate,
+                      },
+                    },
+                  },
+                },
+              ],
             },
-            "limit": 5000
-          }
+            limit: 5000,
+          },
         },
-        "searchQuery": "",
-        "userTimeZone": "Europe/Warsaw"
-      }
-    }
+        searchQuery: '',
+        userTimeZone: 'Europe/Warsaw',
+      },
+    };
 
-    return await axios("https://www.notion.so/api/v3/queryCollection", {
+    return axios('https://www.notion.so/api/v3/queryCollection', {
       method: 'POST',
       data: JSON.stringify(calendarQueryBody),
       headers: {
         'Content-Type': 'application/json',
-        'Cookie': `token_v2=${this.token};`
-      }
+        Cookie: `token_v2=${this.token};`,
+      },
     });
   }
 }
