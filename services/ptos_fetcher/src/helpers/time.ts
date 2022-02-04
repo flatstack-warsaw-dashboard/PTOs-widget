c0a9e87faebba4be2ad5dfca26ee8dbbb14b953e
Kamil Hismatullin <kamil.hismatullin@flatstack.com>
2022-02-04 16:50:48 +0100

    Write PTO data to DynamoDB

---
+++ services/ptos_fetcher/src/helpers/time.ts
@@ -0,0 +1,6 @@
+export function todayPlus(plus: number): string {
+  const today = new Date("2022-01-26");
+  return new Date(today.getFullYear(), today.getMonth(), today.getDate() + plus)
+    .toISOString()
+    .slice(0, 10);
+}
